49aaf736b93e4012f773b158923ade8c68a30124
Khaled <sporshozaman@gmail.com>
2020-03-15 20:08:49 +0600

    View Cart Working

--- routes/cart.js
+++ routes/cart.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const Product = require('../models/Product')
 const Cart = require('../models/Cart')
 
+const loggedin = require('../config/local-authenticator')
+
 router.get('/add/:id', (req, res) => {
     const productId = req.params.id
     const cart = new Cart(req.session.cart ? req.session.cart : {})
@@ -16,6 +18,20 @@ router.get('/add/:id', (req, res) => {
         
         res.redirect('/')
     })
+})
+
+router.get('/details', (req, res) => {
+    // if(!req.session.cart) {
+    //     res.render('shopping-cart', { products: null })
+    // }
+
+    if(!req.session.cart) {
+        console.log('No Cart!');
+        res.render('shopping-cart', { products: null })   
+    } else {
+        var cart = new Cart(req.session.cart)
+        res.render('shopping-cart', { products: cart.generateArray(), totalPrice: cart.totalPrice })
+    }
 })
 
 module.exports = router
