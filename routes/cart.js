482f917e710550b30c5dc6be68b30d6f2a8e8385
Khaled <sporshozaman@gmail.com>
2020-03-21 22:47:24 +0600

    Activated increase by 1

--- routes/cart.js
+++ routes/cart.js
@@ -30,8 +30,25 @@ router.get('/details', (req, res) => {
         res.render('shopping-cart', { products: null })   
     } else {
         var cart = new Cart(req.session.cart)
+        
         res.render('shopping-cart', { products: cart.generateArray(), totalPrice: cart.totalPrice })
     }
+})
+
+router.get('/increase/:id', (req, res) => {
+    const productId = req.params.id
+    const cart = new Cart(req.session.cart ? req.session.cart : {})
+
+    Product.findById(productId, function(err, product) {
+        if(err) {
+            return res.redirect('/')
+        }
+        cart.add(product, product.id)
+        req.session.cart = cart
+        console.log(req.session.cart);
+        
+        res.redirect('/cart/details')
+    })
 })
 
 module.exports = router
